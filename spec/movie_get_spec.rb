6928a9df678b42184f8f8c69e2ac8109ee0a2909
Marquinhos <mvls2001@globo.com>
2018-11-08 14:28:17 -0200

    implementacao consulta movie

--- spec/movie_get_spec.rb
+++ spec/movie_get_spec.rb
@@ -1,10 +1,17 @@
 describe 'GET /movies' do
-    it 'return a movie' do
-        get '/api/movies/5be3329188fb1f075ca34cea'
-        expect_status(200)
-        expect(json_body[:title]).to eql 'Velozes e Furiosos'
+    describe 'status 200' do
+        before(:context) do
+            req = { title: 'Batman Cavaleiro das Trevas', year: 2010 }
+            post '/api/movies', req.to_json
+            @id_livro_valido = json_body[:id]
+        end
+    
+        it 'return a movie' do
+            get '/api/movies/' + @id_livro_valido
+            expect_status(200)
+        end
     end
-
+    
     it 'movie not found' do
         get '/api/movies/5be3329188fb1f075ca34ce4'
         expect_status(404)
