65665c2ee6e6d6103fd4d92b840cf9dc2927ab16
Marquinhos <mvls2001@globo.com>
2018-11-08 14:32:36 -0200

    alteracao

--- spec/movie_get_spec.rb
+++ spec/movie_get_spec.rb
@@ -6,15 +6,18 @@ describe 'GET /movies' do
             @id_livro_valido = json_body[:id]
         end
     
-        it 'return a movie' do
+        it 'return a unique movie' do
             get '/api/movies/' + @id_livro_valido
+            expect_json_types(title: :string, year: :integer)
             expect_status(200)
         end
     end
-    
-    it 'movie not found' do
-        get '/api/movies/5be3329188fb1f075ca34ce4'
-        expect_status(404)
-        expect(json_body).to eql(message: 'Movie Not Found!')
+
+    describe 'status 400' do
+        it 'movie not found' do
+            get '/api/movies/5be3329188fb1f075ca34ce4'
+            expect_status(404)
+            expect(json_body).to eql(message: 'Movie Not Found!')
+        end
     end
 end
